f5a9c7797648ca62fde3c3e30ecc88cd11d88264
IG_RONiT <ronitkene7@gmail.com>
2026-04-26 03:03:18 +0530

    Loader+Errorhandling

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,13 +1,31 @@
 import {useState,useEffect} from  "react";
 
 const API = () =>{
-    const [posts,setPosts ] = useState([])
+    const [posts,setPosts ] = useState([]);
+    const [loading, setLoading ] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
+        setLoading(true);
         fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res)=>res.json())
-        .then((data)=>setPosts(data));
+        .then((res)=>{
+            if(!res.ok){
+                throw new Error("Failed to Load Posts...!")
+            }
+            return res.json()
+        })
+        .then((data)=>{
+            setPosts(data)
+            setLoading(false);
+        })
+        .catch((err)=>{
+            setError(err.message);
+            setLoading(false);
+        });
     },[])
+
+    if(loading) return <h1> Fetching Posts....</h1>;
+    if(error) return <h1>Error : {error}</h1>;
 
     return(
         <div>
